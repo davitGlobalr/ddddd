96a4772ca9560934d6f39056a921b8a38d7aae3c
dkarapetyan <dkarapetyan85@gmail.com>
2026-04-16 13:07:45 +0400

    teet

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
